48b854bc3b5a0c369e9f17f05b46367b87a33bdf
frotage <1013543433@qq.com>
2020-09-17 08:48:42 +0800

    feat:tooltip finish

--- packages/Tooltip/src/index.vue
+++ packages/Tooltip/src/index.vue
@@ -1,103 +1,97 @@
 <template>
-  <popper
-    :trigger="trigger"
-    :options="{
+<popper :trigger="trigger" :options="{
       placement: placement,
       modifiers: { offset: { offset: offset } },
-    }"
-    content="bbb"
-    :visible-arrow="visibleArrow"
-    class="component"
-  >
+    }" content="bbb" :visible-arrow="visibleArrow" :disabled="disabled" class="component">
     <div class="popper">
-      {{ popperText }}
+        {{ popperText }}
     </div>
-    <THButton slot="reference" class="reference" :icon="icon">
-      <slot></slot>
-    </THButton>
-  </popper>
+    <div slot="reference" class="reference">
+        <slot></slot>
+    </div>
+</popper>
 </template>
+
 <script>
 import Popper from "vue-popperjs";
 import "vue-popperjs/dist/vue-popper.css";
-import { oneOf } from "../../../src/utils/index";
-import THButton from "../../Button";
+import {
+    oneOf
+} from "../../../src/utils/index";
 export default {
-  components: {
-    popper: Popper,
-    THButton,
-  },
-  props: {
-    placement: {
-      type: String,
-      validator: (value) => {
-        return oneOf(value, ["left", "right", "top", "bottom"]);
-      },
-      default: "left",
+    components: {
+        popper: Popper,
     },
-    offset: {
-      default: "0,0",
-      type: String,
+    props: {
+        placement: {
+            type: String,
+            validator: (value) => {
+                return oneOf(value, ["left", "right", "top", "bottom"]);
+            },
+            default: "left",
+        },
+        offset: {
+            default: "0,0",
+            type: String,
+        },
+        popperText: {
+            type: String,
+            required: true,
+        },
+        disabled: {
+            type: Boolean,
+        },
+        trigger: {
+            type: String,
+            validator: (value) => {
+                return oneOf(value, [
+                    "hover",
+                    "clickToOpen",
+                    "clickToToggle",
+                    "click",
+                    "focus",
+                ]);
+            },
+        },
+        visibleArrow: {
+            type: Boolean,
+            default: true,
+        },
+        icon: {
+            type: String,
+            default: "icon5",
+        },
     },
-    popperText: {
-      //   default:'popperText',
-      type: String,
-      required: true,
-    },
-    disabled: {
-      type: Boolean,
-    },
-    trigger: {
-      type: String,
-      validator: (value) => {
-        return oneOf(value, [
-          "hover",
-          "clickToOpen",
-          "clickToToggle",
-          "click",
-          "focus",
-        ]);
-      },
-    },
-    visibleArrow: {
-      type: Boolean,
-      default: true,
-    },
-    icon: {
-      type: String,
-      default: "icon5",
-    },
-  },
 };
 </script>
+
 <style lang="less" scoped>
 .component {
     margin: 10px;
 }
+
 // 触发节点
 .reference {
-  // color: #20b2aa;
-  display: inline-block;
-  //   min-height: 30px;
-  //   width: 60px;
-  text-align: center;
-  vertical-align: middle;
+    display: inline-block;
+    text-align: center;
+    vertical-align: middle;
 }
+
 // 弹窗
 .popper {
-  color: #20b2aa;
-  background-color: #fff;
-  border: none;
-  display: inline-block;
-  min-height: 30px;
-  min-width: 70px;
-  font-size: 12px;
-  vertical-align: middle;
-  line-height: 30px;
-  box-shadow: 0px 0px 10px rgba(26, 171, 182, 0.9);
-  /*考虑浏览器兼容性*/
-  -moz-box-shadow: 0px 0px 10px rgba(26, 171, 182, 0.9);
-  -webkit-box-shadow: 0px 0px 10px rgba(26, 171, 182, 0.9);
-  padding: 0 10px;
+    color: #20b2aa;
+    background-color: #fff;
+    border: none;
+    display: inline-block;
+    min-height: 30px;
+    min-width: 70px;
+    font-size: 12px;
+    vertical-align: middle;
+    line-height: 30px;
+    box-shadow: 0px 0px 10px rgba(26, 171, 182, 0.9);
+    /*考虑浏览器兼容性*/
+    -moz-box-shadow: 0px 0px 10px rgba(26, 171, 182, 0.9);
+    -webkit-box-shadow: 0px 0px 10px rgba(26, 171, 182, 0.9);
+    padding: 0 10px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="component">
+<div class="component">
     <div>
-      <THTooptip popperText="hdkjasnjdsan" placement="right">aaa</THTooptip>
-      <THTooptip popperText="hdkjasnjdsan" placement="top">aaa</THTooptip>
-      <THTooptip popperText="hdkjasnjdsan" placement="bottom">aaa</THTooptip>
-      <THTooptip popperText="hdkjasnjdsan">aaa</THTooptip>
+        <THTooptip popperText="hdkjasnjdsan" placement="right">aaa</THTooptip>
+        <THTooptip popperText="hdkjasnjdsan" placement="top">aaa</THTooptip>
+        <THTooptip placement="bottom" popperText="hdkjasnjdsan">
+            <THButton icon="icon1" @click="test">testMessage</THButton>
+        </THTooptip>
     </div>
     <!-- <div>
       <THBackTop icon="icon6"></THBackTop>
     </div>
     <div>
-      <THButton icon="icon6" @click="testMsg">testMessage</THButton>
+
       <THButton icon="icon6" @click="testWarn">testMessage</THButton>
       <THButton icon="icon6" @click="testError">testMessage</THButton>
       <THButton icon="icon6" @click="testSuccess">testMessage</THButton>
@@ -73,11 +74,11 @@
         <div slot="body">今天天气不错</div>
       </THCard>
     </div> -->
-  </div>
+</div>
 </template>
 
 <script>
-// import THButton from "../../packages/Button/index";
+import THButton from "../../packages/Button/index";
 // import THIcon from "../../packages/Icon/index";
 // import THInput from "../../packages/Input/index";
 // import THCard from "../../packages/Card/index";
@@ -92,55 +93,55 @@
 import THTooptip from "../../packages/Tooltip/src/index";
 
 export default {
-  name: "HelloWorld",
-  components: {
-    // THButton,
-    // THIcon,
-    // THInput,
-    // THCard,
-    // THRadio,
-    // THModal,
-    // THAlert,
-    // THCheckbox,
-    // THCheckboxGroup,
-    // // THMessage,
-    // THBackTop,
-    THTooptip,
-  },
-  data() {
-    return {
-      testModal: false,
-      checkbox: [],
-      checkbox1: "",
-      testString: "aaa",
-    };
-  },
-  props: {
-    msg: String,
-  },
-  methods: {
-    test() {
-      console.log("test");
+    name: "HelloWorld",
+    components: {
+        THButton,
+        // THIcon,
+        // THInput,
+        // THCard,
+        // THRadio,
+        // THModal,
+        // THAlert,
+        // THCheckbox,
+        // THCheckboxGroup,
+        // // THMessage,
+        // THBackTop,
+        THTooptip,
     },
-    modalMethod() {
-      this.testModal = true;
+    data() {
+        return {
+            testModal: false,
+            checkbox: [],
+            checkbox1: "",
+            testString: "aaa",
+        };
     },
-    checkboxMethod() {
-      console.log(this.checkbox);
+    props: {
+        msg: String,
     },
-    testMsg() {
-      this.$message.message("test");
+    methods: {
+        test() {
+            console.log("test");
+        },
+        modalMethod() {
+            this.testModal = true;
+        },
+        checkboxMethod() {
+            console.log(this.checkbox);
+        },
+        testMsg() {
+            this.$message.message("test");
+        },
+        testWarn() {
+            this.$message.warn("test");
+        },
+        testError() {
+            this.$message.error("test");
+        },
+        testSuccess() {
+            this.$message.success("testndsoandsakddsnajdnajk");
+        },
     },
-    testWarn() {
-      this.$message.warn("test");
-    },
-    testError() {
-      this.$message.error("test");
-    },
-    testSuccess() {
-      this.$message.success("testndsoandsakddsnajdnajk");
-    },
-  },
 };
 </script>
 
